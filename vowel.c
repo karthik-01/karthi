0a29c16ca8b4ce5eb8f9fb96827da2d9fe3d9e86
karthik-01 <47473917+karthik-01@users.noreply.github.com>
2019-02-09 16:03:57 +0530

    Update vowel.c

--- vowel.c
+++ vowel.c
@@ -2,7 +2,6 @@
 int main()
 {
     char c;
-    printf("Enter the string :");
     scanf("%s",&c);
     if(c=='a'||c=='e'||c=='i'||c=='o'||c=='u'||c=='A'||c=='E'||c=='I'||c=='O'||c=='U')
     printf("Vowel");
