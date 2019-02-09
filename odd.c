61d973962a2938cb1a03c732957c208349a99eec
karthik-01 <47473917+karthik-01@users.noreply.github.com>
2019-02-09 16:17:00 +0530

    Update odd.c

--- odd.c
+++ odd.c
@@ -5,9 +5,9 @@ int main()
     scanf("%d",&n);
     if(n%2==0)
     printf("Even");
-    if else(n<0)
+    else if(n<0)
     printf("invalid");
     else
-    printf("odd");
+    printf("Odd");
 
 }
